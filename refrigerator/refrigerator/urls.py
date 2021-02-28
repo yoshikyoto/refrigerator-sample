d90d9be45e94d9f53fe771c5421c8d97081031d2
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2021-02-28 22:10:29 +0900

    静的なページの作成

--- refrigerator/refrigerator/urls.py
+++ refrigerator/refrigerator/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from refrigerator_app.views import stocks_list
 
 urlpatterns = [
+    path('stocks/', stocks_list),
     path('admin/', admin.site.urls),
 ]
--- refrigerator/refrigerator_app/views.py
+++ refrigerator/refrigerator_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def stocks_list(request):
+    return render(request, 'stocks/list.html')
