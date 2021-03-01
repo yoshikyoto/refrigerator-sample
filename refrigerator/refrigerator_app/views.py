d762f0b4c3e9747910b72e38d9a703fb7276340e
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2021-03-01 12:12:39 +0900

    在庫を表示

--- refrigerator/refrigerator_app/views.py
+++ refrigerator/refrigerator_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from refrigerator_app.models import Stock
 
 
 def stocks_list(request):
-    return render(request, 'stocks/list.html')
+    stocks = Stock.objects.all()
+    return render(request, 'stocks/list.html', {"stocks": stocks})
